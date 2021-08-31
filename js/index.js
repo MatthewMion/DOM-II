1ae8ffc508c30bee0106dd37213658512b0e90ce
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-08-31 15:22:45 -0400

    finished the 10/10 listeners and stopping the nav from reloading

--- js/index.js
+++ js/index.js
@@ -1,66 +1,62 @@
 // #1 - mouseover event on header intro image
 const headerImg = document.querySelector('header img')
 headerImg.addEventListener('mouseover', event => event.target.style.border = 'solid red 3px')
+
+//#2 mouseout on header intro image
 headerImg.addEventListener('mouseout', event => event.target.style.border = '')
 
 
-//#2 - keydown event on body
+//#3 - keydown event on body
 const bodyEvent = document.querySelector('body')
 bodyEvent.addEventListener('keydown', event => event.target.style.backgroundColor = 'purple')
 
-//#3 - dblclick event on html
+//#4 keyup event on body
+bodyEvent.addEventListener('keyup', event => event.target.style.backgroundColor = 'blue')
 
-const dblclickHTMLEvent = document.querySelector('html')
-dblclickHTMLEvent.addEventListener('dblclick', event => window.alert('Do it again'))
+//#5 - dblclick event on html
+const dblclickBodyEvent = document.querySelector('body')
+dblclickBodyEvent.addEventListener('dblclick', event => {
+    window.alert('You have reset the background color')
+    event.target.style.backgroundColor = 'white'
+})
 
-//#4 - focus on content section image
-
-// NEED TO FINISH
-
-const contentSectionP = document.querySelector('.content-section p')
-contentSectionP.addEventListener('focus', event => event.target.style.transform = 'rotate(75deg)')
-
-//#5 load event on window
+//#6 load event on window
 window.addEventListener('load', event => window.alert('Page is fully loaded!') )
 
-//#6 click even for 'Sign Me Up' buttons'
-
+//#7 click even for 'Sign Me Up' buttons'
 const signMeUp = document.querySelectorAll('.btn')
 signMeUp.forEach(item => item.addEventListener('click', event => prompt('Please Enter Your Name')))
 
-//#7 - dragstart event
+//#8 - dragstart event
 const dragOverEvent = document.querySelector('.inverse-content')
 dragOverEvent.addEventListener('dragstart', event => {
     alert('Hey! Dont touch that!')
     event.stopImmediatePropagation()
 })
 
-//#8 - resize event
+//#9 - resize event
 const bodyBackground = document.querySelector('body')
 window.addEventListener('resize', () => bodyBackground.style.color = 'green')
 
-//#9 - 
- 
+ //#10 - mousedown on content section image
+const contentImg = document.querySelector('.content-section img')
+contentImg.addEventListener('mousedown', event => event.target.style.transform = 'scale(2)')
 
-//#10 - wheel event on 'Welcome to fun bus!'
+//#11 - mouseup on content section
+contentImg.addEventListener('mouseup', event => event.target.style.transform = 'scale(1)')
 
+
+//#12 - wheel event on 'Welcome to fun bus!'
 function zoom(event) {
     event.preventDefault();
-  
     scale += event.deltaY * -0.01;
-  
-    // Restrict scale
     scale = Math.min(Math.max(.125, scale), 4);
-  
-    // Apply scale transform
-    el.style.transform = `scale(${scale})`;
+    introH2.style.transform = `scale(${scale})`;
   }
-  
   let scale = 1;
-  const el = document.querySelector('.intro h2');
-  el.onwheel = zoom;
+  const introH2 = document.querySelector('.intro h2');
+  introH2.addEventListener('wheel', zoom)
 
   //nav prevent default
-
   const navItems = document.querySelectorAll(nav)
   navItems.forEach(item => item.addEventListener(event => event.preventDefault()))
