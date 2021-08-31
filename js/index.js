e0651105cd8e48d80049898ddbf92332704cdc87
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-08-31 13:20:19 -0400

    finished 6/10 listeners

--- js/index.js
+++ js/index.js
@@ -14,13 +14,20 @@ dblclickEvent.addEventListener('dblclick', event => window.alert('Do it again'))
 
 //#4 - focus on content section image
 
-focusEvent = document.querySelector('.content-section p')
+// NEED TO FINISH
+
+const focusEvent = document.querySelector('.content-section p')
 focusEvent.addEventListener('focus', event => event.target.style.transform = 'rotate(75deg)')
 
 //#5 load event on window
 window.addEventListener('load', event => window.alert('Page is fully loaded!') )
 
+//#6 click even for 'Sign Me Up' buttons'
 
+const clickEvent = document.querySelectorAll('.btn')
+// clickEvent.addEventListener('click', event => prompt('Please enter your name'))
+clickEvent.forEach(item => item.addEventListener('click', event => prompt('Please Enter Your Name')))
+//#7 
 
 
 
@@ -44,3 +51,5 @@ function zoom(event) {
   let scale = 1;
   const el = document.querySelector('.intro h2');
   el.onwheel = zoom;
+
+  //
