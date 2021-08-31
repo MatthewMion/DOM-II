a9391fd52516a571096a3a2204a176dfc11ae416
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-08-31 14:40:11 -0400

    finished 8/10 listeners

--- js/index.js
+++ js/index.js
@@ -1,40 +1,48 @@
 // #1 - mouseover event on header intro image
-const mouseoverEvent = document.querySelector('header img')
-mouseoverEvent.addEventListener('mouseover', event => event.target.style.border = 'solid red 3px')
+const headerImg = document.querySelector('header img')
+headerImg.addEventListener('mouseover', event => event.target.style.border = 'solid red 3px')
+headerImg.addEventListener('mouseout', event => event.target.style.border = '')
 
 
 //#2 - keydown event on body
-const keydownEvent = document.querySelector('body')
-keydownEvent.addEventListener('keydown', event => event.target.style.backgroundColor = 'purple')
+const bodyEvent = document.querySelector('body')
+bodyEvent.addEventListener('keydown', event => event.target.style.backgroundColor = 'purple')
 
-//#3 - dblclick event on 'html!'
+//#3 - dblclick event on html
 
-const dblclickEvent = document.querySelector('html')
-dblclickEvent.addEventListener('dblclick', event => window.alert('Do it again'))
+const dblclickHTMLEvent = document.querySelector('html')
+dblclickHTMLEvent.addEventListener('dblclick', event => window.alert('Do it again'))
 
 //#4 - focus on content section image
 
 // NEED TO FINISH
 
-const focusEvent = document.querySelector('.content-section p')
-focusEvent.addEventListener('focus', event => event.target.style.transform = 'rotate(75deg)')
+const contentSectionP = document.querySelector('.content-section p')
+contentSectionP.addEventListener('focus', event => event.target.style.transform = 'rotate(75deg)')
 
 //#5 load event on window
 window.addEventListener('load', event => window.alert('Page is fully loaded!') )
 
 //#6 click even for 'Sign Me Up' buttons'
 
-const clickEvent = document.querySelectorAll('.btn')
-// clickEvent.addEventListener('click', event => prompt('Please enter your name'))
-clickEvent.forEach(item => item.addEventListener('click', event => prompt('Please Enter Your Name')))
-//#7 
+const signMeUp = document.querySelectorAll('.btn')
+signMeUp.forEach(item => item.addEventListener('click', event => prompt('Please Enter Your Name')))
 
+//#7 - dragstart event
+const dragOverEvent = document.querySelector('.inverse-content')
+dragOverEvent.addEventListener('dragstart', event => {
+    alert('Hey! Dont touch that!')
+    event.stopImmediatePropagation()
+})
 
+//#8 - resize event
+const bodyBackground = document.querySelector('body')
+window.addEventListener('resize', () => bodyBackground.style.color = 'green')
 
+//#9 - 
+ 
 
-
-
-  //#10 - wheel event on 'Welcome to fun bus!'
+//#10 - wheel event on 'Welcome to fun bus!'
 
 function zoom(event) {
     event.preventDefault();
@@ -52,4 +60,7 @@ function zoom(event) {
   const el = document.querySelector('.intro h2');
   el.onwheel = zoom;
 
-  //
+  //nav prevent default
+
+  const navItems = document.querySelectorAll(nav)
+  navItems.forEach(item => item.addEventListener(event => event.preventDefault()))
